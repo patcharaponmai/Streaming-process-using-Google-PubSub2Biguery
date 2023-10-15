a4dfdaf642cc93b5b2515d6a522cc49fcc545a62
Patcharapon <140698887+patcharaponmai@users.noreply.github.com>
2023-10-16 00:30:52 +0700

    Add files via upload

--- source_code/Data_streaming.py
+++ source_code/Data_streaming.py
@@ -1,49 +1,43 @@
 import sys
 import configparser
+import time
+import json
 from dotenv import load_dotenv
-from google.cloud import bigquery
-from google.cloud import pubsub_v1
+from google.cloud import bigquery, pubsub_v1
+from google.cloud.exceptions import exceptions
+from google.api_core.exceptions import AlreadyExists
 
-def stream_to_bigquery():
-    pass
-
-if __name__ == '__main__':
-
-    # Load environment
-    load_dotenv()
-
-    # Initialize the configuration parser
-    config = configparser.ConfigParser()
-
-    # Read configuration file
-    config.read("./config.ini")
-
+def stream_to_bigquery(message, table):
+    """
+    Triggered by a message published on the Pub/Sub topic
+    """
     try:
-        project_id = config.get('PROJ_CONF', 'PROJ_ID')
-        subscription_name = config.get('PROJ_CONF', 'PUBSUB_TOPIC_NAME')
-        dataset_id = config.get('PROJ_CONF', 'DATASET_NAME')
-        table_id = config.get('PROJ_CONF', 'TABLE_NAME')
+        data = json.loads(message.data.decode('utf-8'))
+
+        # Load data into BigQuery
+        client.insert_rows(table, [data])
+        
+        # Acknowledge the message to remove it from the subscription
+        message.ack()
 
     except Exception as e:
-        print("Error: Cannot get a require parameters.")
-        print(e)
-        sys.exit(1)
+        print(f"Error processing and streaming data to BigQuery: {e}")
 
-    # Initialize the BigQuery client
-    try:
-        client = bigquery.Client(project=project_id)
-    except Exception as e:
-        print("Error: Cannot create connection with BigQuery client")
-        print(e)
-        sys.exit(1)
+def main():
+
+    print("======================================")
+    print("======== Start Streaming Data ========")
+    print("======================================")
+    print()
 
     # Create dataset
     try:
         dataset = bigquery.Dataset(f"{project_id}.{dataset_id}")
         dataset = client.create_dataset(dataset, exists_ok=True)
+    except AlreadyExists:
+        print(f"Dataset '{dataset}' already exists.")
     except Exception as e:
-        print("Error: Cannot create DataSet in BigQuery")
-        print(e)
+        print(f"Error cannot create DataSet in BigQuery {e}")
         sys.exit(1)
 
     # Define table schema
@@ -58,25 +52,107 @@ if __name__ == '__main__':
         bigquery.SchemaField('event_time', 'TIMESTAMP'),
     ]
 
-    # Create table
+    # Create table in BigQuery
+    table_ref = client.dataset(dataset_id).table(table_id)
+
     try:
-        table = bigquery.Table(f"{project_id}.{dataset_id}.{table_id}", schema=schema)
+        table = client.get_table(table_ref)
+        print(f"Table '{table_id}' already exists.")
+    except exceptions.NotFound:
+        table = bigquery.Table(table_ref, schema=schema)
         table = client.create_table(table)
+        print(f"Table '{table_id}' has been created.")
     except Exception as e:
-        print("Error: Cannot create Table in BigQuery")
-        print(e)
+        print(f"Error creating BigQuery table: {e}")
+        sys.exit(1)
+    
+    time.sleep(10)
+
+    # Create a fully-qualified topic path
+    topic_path = publisher.topic_path(project=project_id, topic=pubsub_topic)
+
+    # Create a fully-qualified subscription path
+    subscription_path = subscriber.subscription_path(project=project_id, subscription=pubsub_subscription)
+
+    # Create Pub/Sub topic if it doesn't exist
+    try:
+        publisher.create_topic(name=topic_path)
+        print(f"Pub/Sub Topic has been created")
+    except AlreadyExists:
+        print(f"Pub/Sub topic '{pubsub_topic}' already exists.")
+    except Exception as e:
+        print(f"Error creating Pub/Sub topic: {e}")
         sys.exit(1)
 
-    # Initialize the Subscription client
+    time.sleep(10)
+
+    # Create Pub/Sub subscription if it doesn't exist
     try:
+        subscriber.create_subscription(name=subscription_path, topic=topic_path, ack_deadline_seconds=300)
+        print(f"Pub/Sub Subscription has been created")
+    except AlreadyExists:
+        print(f"Pub/Sub subscription '{pubsub_subscription}' already exists.")
+    except Exception as e:
+        print(f"Error creating Pub/Sub subscription: {e}")
+        sys.exit(1)
+
+    def callback(message):
+        stream_to_bigquery(message, table)
+
+    # Consuming data from Pub/Sub topic and ingest into BigQuery
+    print(f"Listening for messages on {subscription_path}..")
+    subscriber.subscribe(subscription=subscription_path, callback=callback)
+    
+    # Keep the script running to continue processing messages
+    try:
+        while True:
+            time.sleep(30)  # You can adjust the sleep interval as needed
+            print("Consuming message is in progress ...")
+    except KeyboardInterrupt:
+        print()
+        print("=======================================")
+        print("======= Complete Streaming Data =======")
+        print("=======================================")
+
+
+######################################
+############ MAIN PROGRAM ############
+######################################
+
+if __name__ == '__main__':
+
+    # Load environment
+    load_dotenv()
+
+    # Initialize the configuration parser
+    config = configparser.ConfigParser()
+
+    # Read configuration file
+    config.read("./config.ini")
+
+    try:
+        project_id = config.get('PROJ_CONF', 'PROJ_ID')
+        pubsub_topic = config.get('PROJ_CONF', 'PUBSUB_TOPIC_NAME')
+        pubsub_subscription = config.get('PROJ_CONF', 'PUBSUB_SUBSCRIPTION_NAME')
+        dataset_id = config.get('PROJ_CONF', 'DATASET_NAME')
+        table_id = config.get('PROJ_CONF', 'TABLE_NAME')
+    except Exception as e:
+        print(f"Error cannot get require parameters: {e}")
+        sys.exit(1)
+
+    # Initialize Pub/Sub Publisher and Subscriber client
+    try:
+        publisher = pubsub_v1.PublisherClient()
         subscriber = pubsub_v1.SubscriberClient()
     except Exception as e:
-        print("Error: Cannot create connection with BigQuery client")
-        print(e)
+        print(f"Error cannot create connection with Pub/Sub client: {e}")
         sys.exit(1)
 
-    # # Define subscription path to interact with Pub/Sub topic
-    # subscription_path = subscriber.subscription_path(project=project_id, subscription=subscription_name)
+    # Initialize the BigQuery client
+    try:
+        client = bigquery.Client(project=project_id)
+    except Exception as e:
+        print(f"Error cannot create connection with BigQuery client: {e}")
+        sys.exit(1)
 
-    # # Consuming data from Pub/Sub topic and ingest into BigQuery
-    # subscriber.subscribe(subscription=subscription_path, callback=stream_to_bigquery)
+    main()
--- source_code/Generate_mock_data.py
+++ source_code/Generate_mock_data.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 from faker import Faker
 from google.cloud import pubsub_v1
+from google.api_core.exceptions import AlreadyExists
 
 def get_event_id():
     unique_id = uuid.uuid4()
@@ -46,6 +47,55 @@ def get_eventtime():
 
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
+def main():
+
+    print("======================================")
+    print("====== Start Generate Mock Data ======")
+    print("======================================")
+    print()
+
+    # Create a fully-qualified topic path
+    topic_path = publisher.topic_path(project=project_id, topic=pubsub_topic)
+
+    number = 0
+
+    try:
+        # Generate and publish mock data to Pub/Sub
+        while True:
+
+            delay_time = random.randint(2,3)
+            time.sleep(delay_time)
+
+            eventname = get_event()
+            item_qty = get_item_quantity(eventname)
+
+            event = {
+                'No': number,
+                'event_id': get_event_id(),
+                'name': get_name(),
+                'event_name': eventname,
+                'category': get_category(),
+                'item_id': get_item_id(),
+                'item_quantity': item_qty,
+                'event_time': get_eventtime(),
+            }
+
+            print(json.dumps(event))
+            number+=1
+            
+            # Publish the mock data to the Pub/Sub topic
+            try:
+                publisher.publish(topic_path, json.dumps(event).encode('utf-8'))
+            except Exception as e:
+                print(f"Error cannot publish data: {e}")
+                sys.exit(1)
+        
+    except KeyboardInterrupt as e:
+        print()
+        print("=======================================")
+        print("===== Complete Generate Mock Data =====")
+        print("=======================================")
+
 ######################################
 ############ MAIN PROGRAM ############
 ######################################
@@ -64,68 +114,18 @@ if __name__ == '__main__':
     try:
         project_id = config.get('PROJ_CONF', 'PROJ_ID')
         pubsub_topic = config.get('PROJ_CONF', 'PUBSUB_TOPIC_NAME')
-
     except Exception as e:
-        print("Error: Cannot get a require parameters.")
-        print(e)
+        print(f"Error cannot get require parameters: {e}")
         sys.exit(1)
-    
-    topic_name = f"projects/{project_id}/topics/{pubsub_topic}"
 
-    # print(topic_name)
-
-    print("======================================")
-    print("====== Start Generate Mock Data ======")
-    print("======================================")
-    print()
-
-    # Initialize Pub/Sub client
+    # Initialize Pub/Sub Publisher client
     try:
         publisher = pubsub_v1.PublisherClient()
     except Exception as e:
-        print(f"Error: Cannot create connection with Pub/Sub client")
-        print(e)
+        print(f"Error cannot create connection with Pub/Sub client: {e}")
         sys.exit(1)
-
+    
     # Initialize the Faker library for generating mock data
     fake = Faker()
-    number = 0
 
-    try:
-        # Generate and publish mock data to Pub/Sub
-        while True:
-
-            delay_time = random.randint(0,1)
-            time.sleep(delay_time)
-
-            eventname = get_event()
-            item_qty = get_item_quantity(eventname)
-
-            event = {
-                'No.': number,
-                'event_id': get_event_id(),
-                'name': get_name(),
-                'event_name': eventname,
-                'category': get_category(),
-                'item_id': get_item_id(),
-                'item_quantity': item_qty,
-                'event_time': get_eventtime(),
-            }
-
-            print(json.dumps(event))
-            number+=1
-            
-            # # Publish the mock data to the Pub/Sub topic
-            try:
-                publisher.publish(topic_name, json.dumps(event).encode('utf-8'))
-            except Exception as e:
-                print(f"Error: {e}")
-
-            sys.exit(1)
-        
-    except KeyboardInterrupt as e:
-        print()
-        print("=======================================")
-        print("===== Complete Generate Mock Data =====")
-        print("=======================================")
-
+    main()
